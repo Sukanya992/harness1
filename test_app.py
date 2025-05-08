8f83667cbcda13d2924c9d9504926f4d5690d717
Sukanya992 <sukanya09200206@gmail.com>
2025-05-08 10:53:22 +0530

    Delete test_app.py

--- test_app.py
+++
@@ -1,8 +0,0 @@
-# test_app.py
-from app import app
-
-def test_home():
-    tester = app.test_client()
-    response = tester.get('/')
-    assert response.status_code == 200
-    assert b"Hello, world from Dockerized Flask app!" in response.data
