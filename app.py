e0f2275226cfce21aa4cd12a2d56bee0f10708df
Sukanya992 <sukanya09200206@gmail.com>
2025-05-26 10:44:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
 from flask import Flask
-from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
-# Disable CSRF protection via config
-app.config['WTF_CSRF_ENABLED'] = False
-
-csrf = CSRFProtect(app)
-
 @app.route("/")
 def hello():
-    return "Hello, world from Dockerized Flask app!"
+    return "Hello,from Harness !"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
