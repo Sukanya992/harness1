905bb524f76d5a74544d1f8837d588c472aab60b
Sukanya992 <sukanya09200206@gmail.com>
2025-05-26 10:56:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from flask import Flask
-from flask_wtf import CSRFProtect
 
 app = Flask(__name__)
-app.config['WTF_CSRF_ENABLED'] = False  # disables CSRF
-csrf = CSRFProtect(app)
 
 @app.route("/")
 def hello():
-    return "Hello with Harness"
+    return "Hello from Harness!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
